0ca881e052bbd2a5390852a6dc3d48b0800dbf53
diegonc <diegonc@duriel>
2014-10-04 19:46:23 -0300

    Objeto Caja - Se agregan las normales al objeto.

--- public_html/js/caja.js
+++ public_html/js/caja.js
@@ -12,13 +12,14 @@ function Caja(cortesPorCara, franjas) {
     var PASO = 1 / (cortesPorCara - 1);
 
     this.caja = [];
+    this.normales = [];
     this.indices = [];
 
     // La caja esta formada por cortesPorCara vertices en cada cara.
     // Esto se representa por una grilla de
-    // (cortesPorCara + 3 x (cortesPorCara - 1) x (FRANJAS + 1 + 2)
-    //= (4 x cortesPorCara - 3) x (FRANJAS + 1 + 2
-    var verticesPorCol = 4 * cortesPorCara - 3;
+    // 4 x cortesPorCara x (FRANJAS + 1 + 2)
+    //= 4 x cortesPorCara x (FRANJAS + 1 + 2)
+    var verticesPorCol = 4 * cortesPorCara;
     this.indices = generarIndexBuffer(verticesPorCol, (franjas + 3));
     
     // La primer fila de la grilla corresponde al
@@ -27,27 +28,39 @@ function Caja(cortesPorCara, franjas) {
         this.caja.push(0.5);
         this.caja.push(0.5);
         this.caja.push(Z_MAX);
+        
+        this.normales.push(0);
+        this.normales.push(0);
+        this.normales.push(1);
     }
     // Las (FRANJAS + 1) filas intermedias corresponden
     // al cuerpo de la caja.
     
     // Primero se calculan la coordenadas x,y de una franja
     var verticesFranja = [];
+    var normalesFranja = [];
     var x = 0;
     var y = 0;
-    verticesFranja.push([x, y]);
-    for (var i=0; i < (verticesPorCol - 1); i++) {
-        var cara = Math.floor(i / (cortesPorCara - 1));
+    for (var i=0; i < verticesPorCol; i++) {
+        var cara = Math.floor(i / cortesPorCara);
         if (cara === 0) {
-            x = ((i % (cortesPorCara - 1)) + 1) * PASO;
+            x = (i % cortesPorCara) * PASO;
         } else if (cara === 1) {
-            y = ((i % (cortesPorCara - 1)) + 1) * PASO;
+            y = (i % cortesPorCara) * PASO;
         } else if (cara === 2) {
-            x = 1 - (((i % (cortesPorCara - 1)) + 1) * PASO);
+            x = 1 - ((i % cortesPorCara) * PASO);
         } else if (cara === 3) {
-            y = 1 - (((i % (cortesPorCara - 1)) + 1) * PASO);
+            y = 1 - ((i % cortesPorCara) * PASO);
         }
         verticesFranja.push([x, y]);
+        if (cara === 0)
+            normalesFranja.push([0, -1]);
+        else if (cara === 1)
+            normalesFranja.push([1, 0]);
+        else if (cara === 2)
+            normalesFranja.push([0, 1]);
+        else if (cara === 3)
+            normalesFranja.push([-1, 0]);
     }
     
     // Luego se añaden los vertices con la coordenada z de cada franja
@@ -59,6 +72,11 @@ function Caja(cortesPorCara, franjas) {
             this.caja.push(pos[0]);
             this.caja.push(pos[1]);
             this.caja.push(z);
+            
+            var norm = normalesFranja[j];
+            this.normales.push(norm[0]);
+            this.normales.push(norm[1]);
+            this.normales.push(0);
         }
     }
     // La ultima fila de la grilla corresponde al
@@ -67,6 +85,10 @@ function Caja(cortesPorCara, franjas) {
         this.caja.push(0.5);
         this.caja.push(0.5);
         this.caja.push(0.0);
+        
+        this.normales.push(0);
+        this.normales.push(0);
+        this.normales.push(-1);
     }
 
     this.colors = [];
@@ -101,6 +123,10 @@ Caja.prototype.initGL = function(gl) {
     this.colorBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, this.colorBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.STATIC_DRAW);
+    
+    this.normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.normales), gl.STATIC_DRAW);
 
     this.indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);

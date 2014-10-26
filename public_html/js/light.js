80c95e1495cd15950d478a33839c4f779eb6f3a5
diegonc <diegonc@duriel>
2014-10-26 13:39:18 -0300

    Light - Metodo applyCamera

--- public_html/js/light.js
+++ public_html/js/light.js
@@ -1,5 +1,10 @@
 function Light(ambientColor, position, directionalColor) {
     this.ambientColor = ambientColor;
-    this.position = position;
+    this.worldPos = position;
     this.directionalColor = directionalColor;
+    this.position = vec3.set(position, []);
 }
+
+Light.prototype.applyCamera = function(c) {
+    mat4.multiplyVec3(c, this.worldPos, this.position);  
+};
